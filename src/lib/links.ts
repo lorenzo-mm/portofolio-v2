5762d914f5547ca9b8bb802693bcb3d7526abf76
lorenzo-mm <cepo_mm@hotmail.com>
2024-02-23 17:55:35 +0100

    new links

---
+++ src/lib/links.ts
@@ -0,0 +1,17 @@
+import { useTranslations } from 'next-intl'
+
+
+export const links = [
+  {
+    title: 'Header.present',
+    hash: '/' 
+  },
+  {
+    title: 'Header.skills',
+    hash: '#skills'
+  },
+  {
+    title: 'Header.contact',
+    hash: '#contact'
+  }
+]
