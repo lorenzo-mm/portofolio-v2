6cc4203c821606de5ad88ed5d418ee34c49a974e
lorenzo-mm <cepo_mm@hotmail.com>
2024-03-06 00:47:38 +0100

    new file & move code in the new file

---
+++ src/components/constant/portfolio.ts
@@ -0,0 +1,86 @@
+'use client'
+
+import Apple from '@/../../public/apple1.png'
+import Burger from '@/../../public/burger1.png'
+import Calculadora from '@/../../public/calculadora.png'
+import Spacex from '@/../../public/spacex.png'
+import Admin from '@/../../public/admin.png'
+import Mapfre from '@/../../public/mapfre.png'
+import Pptss from '@/../../public/pptss.png'
+import Todolist from '@/../../public/todolist.png'
+import AgeCalculator from '@/../../public/agecalculator.png'
+
+export const data = [
+  {
+    id: 1,
+    image: Apple,
+    title: Apple,
+    // subtitle: t('portfolio.descripcion_apple'),
+      github: 'https://github.com/lorenzo-mm/Clon-Apple-2021',
+      demo: 'https://lorenzo-mm.github.io/apple-page-clon/Html/'
+    },
+    {
+      id: 2,
+      image: Burger,
+      // title: t('portfolio.burger'),
+      // subtitle: t('portfolio.descripcion_burger'),
+      github: 'https://github.com/lorenzo-mm/Burger-Website',
+      demo: 'https://lorenzo-mm.github.io/Burger-Website/'
+    },
+    {
+      id: 3,
+      image: Calculadora,
+      // title: t('portfolio.calculadora'),
+      // subtitle: t('portfolio.descripcion_calculadora'),
+      github: 'https://github.com/lorenzo-mm/Calculator',
+      demo: 'https://lorenzo-mm.github.io/Calculator/'
+    },
+    {
+      id: 4,
+      image: Spacex,
+      // title: t('portfolio.spacex'),
+      // subtitle: t('portfolio.descripcion_spacex'),
+      github: 'https://github.com/lorenzo-mm/Space-X-API-with-React',
+      demo: 'https://lorenzo-mm.github.io/Space-X-API-with-React/'
+    },
+    {
+      id: 5,
+      image: Admin,
+      // title: t('portfolio.admin'),
+      // subtitle: t('portfolio.descripcion_admin'),
+      github: 'https://github.com/lorenzo-mm/Dashboard-Admin-with-React',
+      demo: 'https://lorenzo-mm.github.io/Dashboard-Admin-with-React/'
+    },
+    {
+      id: 6,
+      image: Mapfre,
+      // title: t('portfolio.mapfre'),
+      // subtitle: t('portfolio.descripcion_mapfre'),
+      github: 'https://github.com/lorenzo-mm/portal-mapfre',
+      demo: 'https://github.com/lorenzo-mm/TechNews.'
+    },
+    {
+      id: 7,
+      image: Pptss,
+      // title: t('portfolio.pptss'),
+      // subtitle: t('portfolio.descripcion_pptss'),
+      github: 'https://github.com/lorenzo-mm/react-rpsls-main',
+      demo: 'https://lorenzo-mm.github.io/react-rpsls-main/'
+    },
+    {
+      id: 8,
+      image: Todolist,
+      // title: t('portfolio.todo_list'),
+      // subtitle: t('portfolio.descripcion_todo_list'),
+      github: 'https://github.com/lorenzo-mm/Todo-List',
+      demo: 'https://lorenzo-mm.github.io/todo-list/'
+    },
+    {
+      id: 9,
+      image: AgeCalculator,
+      // title: t('portfolio.age_calc'),
+      // subtitle: t('portfolio.descripcion_age_calc'),
+      github: 'https://github.com/lorenzo-mm',
+      demo: 'https://lorenzo-mm.github.io/age-calculator-main/'
+    }
+]
